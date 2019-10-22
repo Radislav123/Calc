66a79527ec1041cde16a92b09056ce5035460afe
Radislav123 <forgameslegolas@mail.ru>
2019-10-23 00:23:40 +0300

    tests added

--- src/test/java/DividerTest.java
+++ src/test/java/DividerTest.java
@@ -1,10 +1,10 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.lang.annotation.Annotation;
+import static org.junit.jupiter.api.Assertions.*;
 
-class DividerTest implements BeforeEach, AfterEach {
+class DividerTest implements AfterEach, BeforeEach {
 
 
 	@Override
@@ -13,36 +13,47 @@ class DividerTest implements BeforeEach, AfterEach {
 	}
 
 	@BeforeEach
-	void beforeEach() {
-		System.out.println("beforeEach");
+	void beforeEach(TestInfo testInfo) {
+		System.out.println(testInfo.getDisplayName());
 	}
 
 	@AfterEach
 	void afterEach() {
-		System.out.println("afterEach");
+		String string = "=";
+		for (int i = 0; i < 23; i ++)
+			string = string.concat("=");
+		System.out.println(string + "\n");
 	}
 
-	@ParameterizedTest
+	@DisplayName("zero by zero division test")
+	@Test
+	void zeroByZeroDivisionTest() {
+		float operand1 = 0;
+		float operand2 = 0;
+		assertTrue(Float.isNaN(Divider.count(operand1, operand2)));
+	}
+
+	@DisplayName("test set 1")
+	@ParameterizedTest(name = "{index} test in the 1 test set")
 	@ValueSource(floats = {-2, 0, 2})
 	void countTest1(float operand1) {
 		float operand2 = -2;
-		System.out.println(operand1 + " / " + operand2);
-		System.out.println("response = " + Divider.count(operand1, operand2));
+		assertTrue(operand1 / operand2 == Divider.count(operand1, operand2));
 	}
 
-	@ParameterizedTest
-	@ValueSource(floats = {-2, 0, 2})
+	@DisplayName("test set 2")
+	@ParameterizedTest(name = "{index} test in the 2 test set")
+	@ValueSource(floats = {-2, 2})
 	void countTest2(float operand1) {
 		float operand2 = 0;
-		System.out.println(operand1 + " / " + operand2);
-		System.out.println("response = " + Divider.count(operand1, operand2));
+		assertTrue(operand1 / operand2 == Divider.count(operand1, operand2));
 	}
 
-	@ParameterizedTest
+	@DisplayName("test set 3")
+	@ParameterizedTest(name = "{index} test in the 3 test set")
 	@ValueSource(floats = {-2, 0, 2})
 	void countTest3(float operand1) {
 		float operand2 = 2;
-		System.out.println(operand1 + " / " + operand2);
-		System.out.println("response = " + Divider.count(operand1, operand2));
+		assertTrue(operand1 / operand2 == Divider.count(operand1, operand2));
 	}
 }
